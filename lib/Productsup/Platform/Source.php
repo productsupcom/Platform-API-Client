7b85f6f937840b30551263b59d052b4d0a96bbc5
Chris Sachs <cs@productsup.com>
2015-03-12 15:38:31 +0100

    add support for site sources, ISSUE-467

---
+++ lib/Productsup/Platform/Source.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Productsup\Platform;
+
+class Source extends DataModel {
+    const TYPE_DEFAULT = 1;
+    const TYPE_ADDITIONAL = 2;
+
+    public $id;
+    public $site_id;
+    /** @var string url to import source */
+    public $source;
+    /** @var int one of the TYPE_* constants */
+    public $import_type = self::TYPE_DEFAULT;
+}
